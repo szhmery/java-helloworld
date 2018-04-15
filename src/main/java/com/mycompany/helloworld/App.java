ab0da98a291a7a21328316ed7b5255e7c065f7ed
szhmery <szhmery@gmail.com>
2018-04-15 20:30:26 +0800

    init commit

---
+++ src/main/java/com/mycompany/helloworld/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!  We are now!" );
+        System.out.println( "You are great!" );
+    }
+}
